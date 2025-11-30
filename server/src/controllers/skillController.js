f557726dcd6020e4ec3bd003ee85376b781181bc
Ali Madan <alimadan1234@gmail.com>
2025-11-30 15:31:32 +0300

    finished the skillControllers

--- server/src/controllers/skillController.js
+++ server/src/controllers/skillController.js
@@ -26,10 +26,52 @@ const ShowSkill = async (req, res) => {
     }
     
 }
+
+const allSkills = async (req, res) => {
+    try {
+        const Skills = await Skills.find()
+        if (Skills.length){
+            res.status(200).json(Skills)
+        }else{
+            res.sendStatus(204)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const deleteSkill = async (req, res) => {
+    try {
+        const skill = await skill.findByIdAndDelete(req.params.id)
+
+        if (skill){
+            res.status(200).json(skill)
+        } else{
+            res.sendStatus(404)
+        }
+    } catch (error) {
+        res.status(500).json({error: error.massage})
+    }
+}
+
+const updateskill = async (req, res) => {
+    try {
+        const skill = await skill.findByIdAndUpdate(req.params.id)
+
+        if (skill){
+            res.status(200).json(skill)
+        } else{
+            res.sendStatus(404)
+        }
+    } catch (error) {
+        res.status(500).json({error: error.massage})
+    }
+}
+
 module.exports = {
     createSkill,
     allSkills,
     ShowSkill,
     deleteSkill,
-    updateSill,
+    updateskill
 }
