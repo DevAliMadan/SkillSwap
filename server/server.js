e215513ec048275d0d9ba0c399db848e44ef83b6
Ali Madan <alimadan1234@gmail.com>
2025-11-30 14:22:38 +0300

    adds the connection and routes also gitignore

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const logger = require('morgan')
+const cors = require('cors')
+
+const skillRoutes = require('./routes/skillRoutes')
+const authRoutes = require('./routes/authRoutes')
+const userRoutes = require('./routes/userRoutes')
+const swapRoutes = require('./routes/swapRoutes')
+const massageRoutes = require('./routes/massageRoutes')
+
+dotenv.config()
+const app = express()
+
+
+
+
+mongoose.connect(process.env.DB_URI)
+mongoose.connection.on('connected', ()=>{
+    console.log('connected to mongoDB')
+})
+
+app.use(cors({origin: 'http://localhost:5173'}))
+app.use(express.json())
+app.use(logger('dev'))
+app.use('/auth', authRoutes)
+app.use('/massage', massageRoutes)
+app.use('/user', userRoutes)
+app.use('/swap', swapRoutes)
+app.use('/skill', skillRoutes)
+
+app.listen(3000, () => {
+    console.log('App is listening!')
+})
