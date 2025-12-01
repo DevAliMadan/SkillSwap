761f6b2fd46855b5ad968b3a19932c0b3f531761
Ali Madan <alimadan1234@gmail.com>
2025-12-01 13:13:05 +0300

    finifhef the Swap model

--- server/src/models/Swap.js
+++ server/src/models/Swap.js
@@ -1,6 +1,82 @@
 const mongoose = require('mongoose');
 
 const swapSchema  = new mongoose.Schema({
+    requester: {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            require: true
+        },
+        skill: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Skill',
+            require: true
+        }
+    },
+    provider: {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            require: true
+        },
+        skill: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Skill',
+            require: true
+        }
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'accepted', 'rejected', 'ongoing', 'completed', 'cancelled'],
+        default: 'pending'
+    },
+    details: {
+        duration: {
+            hours: Number,
+            sessions: Number 
+        },
+        schedule: [{
+            date: Date,
+            duration: Number,
+            completed: {
+                type: Boolean,
+                default: false
+            }
+        }],
+        terms: String,
+        notes: String
+    },
+    message: [{
+        sender: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },
+        content: String,
+        timestamp: {
+            type: Date,
+            default: Date.now
+        }
+    }],
+    reviews: {
+        fromRequester: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Review'
+        },
+        fromProvider: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    },
+    startDate: Date,
+    endDate: Date,
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    udatedAt: {
+        type: Date,
+        default: Date.now,
+    }
 
 })
 
